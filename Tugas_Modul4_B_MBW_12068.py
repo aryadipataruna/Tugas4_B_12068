d0432e05cfcfe21773b47e2f77a053f466c896a5
aryadipataruna <220712068@students.uajy.ac.id>
2024-11-06 15:32:19 +0700

    Update Tugas_Modul4_B_MBW_12068.py

--- Tugas_Modul4_B_MBW_12068.py
+++ Tugas_Modul4_B_MBW_12068.py
@@ -60,7 +60,7 @@ uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["cs
 if uploaded_file is not None:
     input_data = pd.read_csv(uploaded_file)
 
-    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - EFAN</h1>", unsafe_allow_html=True) 
+    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - MBW</h1>", unsafe_allow_html=True) 
     st.dataframe(input_data)
 
     # direktori tempat penyimpanan ketiga model yang telah di dump sebelumnya
